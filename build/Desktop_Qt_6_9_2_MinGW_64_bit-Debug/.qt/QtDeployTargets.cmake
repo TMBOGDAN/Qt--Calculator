6b1abfc66e042807a71a9b2bc13eae2e96fa2c81
Matei Bogdan Tudor <tmateib@gmail.com>
2025-09-22 00:03:51 +0300

    Remove build files from repository

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calculator_FILE C:/Users/tmate/Documents/Calculator/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/Calculator.exe)
-set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calculator_RUNTIME_DLLS C:/Apps/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Apps/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Apps/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
